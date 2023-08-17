8ec7890b14d2a449f18e6a692b176376406fb64f
Lapecru64 <perlura64@outlook.com>
2023-08-17 17:32:58 -0600

    Portafolio completo

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,32 +4,32 @@ import { Link } from 'react-router-dom';
 
 const projects = [
   {
-    src: "/public/imgs/galletasfortuna.jpg",
+    src: "/imgs/galletasfortuna.jpg",
     alt: "Fortune Cookie App",
     link: "https://stupendous-valkyrie-4ca841.netlify.app/"
   },
   {
-    src: "/public/imgs/weather.jpg",
+    src: "/imgs/weather.jpg",
     alt: "Weather App",
     link: "https://64a0d9b26243bb088540b145--deft-croquembouche-7f6f1f.netlify.app/"
   },
   {
-    src: "/public/imgs/RickandMorty.jpg",
+    src: "/imgs/RickandMorty.jpg",
     alt: "Rick and Morty App",
     link: "https://legendary-jelly-89eb1a.netlify.app/"
   },
   {
-    src: "/public/imgs/users.jpg",
+    src: "/imgs/users.jpg",
     alt: "Users App",
     link: "https://warm-donut-52d672.netlify.app/"
   },
   {
-    src: "/public/imgs/pokemon.jpg",
+    src: "/imgs/pokemon.jpg",
     alt: "Pókedex App",
     link: "https://dazzling-melomakarona-63db3c.netlify.app/"
   },
   {
-    src: "/public/imgs/ecommerce.jpg",
+    src: "/imgs/ecommerce.jpg",
     alt: "E-commerce App",
     link: "https://bejewelled-babka-3dfe70.netlify.app/"
   }
@@ -41,7 +41,7 @@ const Portfolio = () => {
       <h1 className="color-font">Portfolio</h1>
       <div className="portfolio-images">
         {projects.map((project, index) => (
-          <Link to={project.link} key={index} target="_blank">
+          <Link to={project.link} key={index}>
             <div className="img-item" style={{ transform: `rotate(${(360 / projects.length) * index}deg)` }}>
               <img className="img" src={project.src} alt={project.alt} />
             </div>
