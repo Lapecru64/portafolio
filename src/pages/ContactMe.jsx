6883686ec2487015e78ba885bd0acbad0d28c011
Lapecru64 <perlura64@outlook.com>
2023-10-12 16:11:37 -0600

    cambios de estilos

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -12,19 +12,19 @@ const ContactMe = () => {
           <ul>
             <li>
               <a href="https://github.com/Lapecru64" target="_blank">
-              <box-icon type='logo' name='github' size="100px"></box-icon>
+              <box-icon type='logo' name='github' size="100px"></box-icon>GITHUB
               </a>
             </li>
             <br/>
             <li>
               <a href="https://www.linkedin.com/in/laura-perez-45560523a/" target="_blank" className="footer__link">
-              <box-icon type='logo' name='linkedin-square' size="100px"></box-icon>
+              <box-icon type='logo' name='linkedin-square' size="100px"></box-icon>LINKEDIN
               </a>
             </li>
             <br/>
             <li>
               <a href="mailto:perlaura64@outlook.com" className="footer__link">
-                <box-icon name='mail-send' size="100px"></box-icon>
+                <box-icon name='mail-send' size="100px"></box-icon>MAIL
               </a>
             </li>
             <br/>
