0b48bdc72183f1679651bc011b3b1cb6c5d184ac
Lapecru64 <perlura64@outlook.com>
2023-10-12 18:56:14 -0600

    Cambios Portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,26 @@ import AboutMe from './pages/AboutMe';
 import MySkills from './pages/MySkills';
 import Portfolio from './pages/Portfolio';
 import ContactMe from './pages/ContactMe';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleDarkMode } from './store/index';
 import DarkModeWrapper from '../src/components/DarkModeWrapper';
+import { Provider } from 'react-redux';
+import store from './store/index';
 import './App.css';
 
 function App() {
-  const dispatch = useDispatch();
-  const darkMode = useSelector((state) => state.counter.darkMode);
-
-  const toggleDark = () => {
-    dispatch(toggleDarkMode());
-  };
-
   return (
-    <div className={`container-principal ${darkMode ? 'dark' : ''}`}>
-      <Header />
-      <button onClick={toggleDark} className="dark-mode-button">
-        Toggle Dark Mode
-      </button>
-      <DarkModeWrapper>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route path="/skills" element={<MySkills />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<ContactMe />} />
-        </Routes>
-      </DarkModeWrapper>
+    <div className="container-principal">
+      <Provider store={store}>
+        <Header />
+        <DarkModeWrapper>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/skills" element={<MySkills />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<ContactMe />} />
+          </Routes>
+        </DarkModeWrapper>
+      </Provider>
     </div>
   );
 }
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,22 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleDarkMode } from '../store/index'; // Ajusta la importación
 
-import { Link } from "react-router-dom"
+import './Header.css';
 
 const Header = () => {
-    return (
-     <header>
-        <Link to="/">
-          <h1>Home</h1>
-        </Link>
-        <nav>
-          <ul>
-            <li><Link to="/about">About Me</Link></li>
-            <li><Link to="/skills">My Skills</Link></li>
-            <li><Link to="/portfolio">Portfolio</Link></li>
-            <li><Link to="/contact">Contact Me</Link></li>
-          </ul>
-        </nav>
-     </header>
-    )
+  const dispatch = useDispatch();
+  const darkMode = useSelector((state) => state.counter.darkMode);
+
+  const toggleDark = () => {
+    dispatch(toggleDarkMode());
+  };
+
+  return (
+    <header className={`header-container ${darkMode ? 'dark' : ''}`}>
+      <Link to="/">
+        <h1>Home</h1>
+      </Link>
+      <nav>
+        <ul className="header-links">
+          <li><Link to="/about">About Me</Link></li>
+          <li><Link to="/skills">My Skills</Link></li>
+          <li><Link to="/portfolio">Portfolio</Link></li>
+          <li><Link to="/contact">Contact Me</Link></li>
+        </ul>
+      </nav>
+      <button onClick={toggleDark} className="dark-mode-button">
+        Toggle Dark Mode
+      </button>
+    </header>
+  );
 }
 
 export default Header;
+
